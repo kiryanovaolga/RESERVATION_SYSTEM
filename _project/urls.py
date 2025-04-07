74fdb700fd7a53a593482bbb9c11f1c21c8eaa09
kiryanovaolga <olgakiryanova55@yandex.ru>
2025-04-07 12:43:44 +0200

    make redirect

--- _project/urls.py
+++ _project/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/reservation/')),
     path('reservation/', include('reservation.urls', namespace='reservation')),
     path('admin/', admin.site.urls),
 
