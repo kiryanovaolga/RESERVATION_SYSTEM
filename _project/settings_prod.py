1cb3d377044569203005cee203752d6de58ab543
kiryanovaolga <olgakiryanova55@yandex.ru>
2025-04-07 19:10:29 +0200

    static

--- _project/settings_prod.py
+++ _project/settings_prod.py
@@ -117,6 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
